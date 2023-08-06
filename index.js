0c5bda4b18122582155a6cb82472cc4562335840
Aliza Sharma <aliza.sharma12@gmail.com>
2023-08-06 16:00:49 -0500

    updated index js

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-const inquirer = require('inquirer')
-const Circle = require('./lib/shapes')
-const Triangle = require('./lib/shapes')
-const Square = require('./lib/shapes')
-
-const circle = new Circle(answers.text, answers.color, answers.textColor)
-shape.render();
+const inquirer = require('inquirer');
+const fs = require('fs');
+const [Circle, Triangle, Square] = require('./lib/shapes');
 
 const triangle = new Triangle(answers.text, answers.color, answers.textColor)
 shape.render();
-
+const circle = new Circle(answers.text, answers.color, answers.textColor)
+shape.render();
 const square = new Square(answers.text, answers.color, answers.textColor)
 shape.render();
+
+let shapeSelect; 
+if(answers.shape === "Triangle"){
+    shapeSelect = new Triangle();
+} else if (answers.shape === "Circle"){
+    shapeSelect = new Circle();
+} else if (answers.shape === "Square"){
+    shapeSelect = new Square();
+}
+
+function init(){
+
+inquirer
+.prompt([
+    {type: 'list',
+    name: 'shape',
+    message: 'what is the desired shape of your svg?',
+    choices: ['circle', 'triangle', 'square']
+    },
+
+    {type: 'input',
+     name: 'color',
+     message:'what is the desired color of your svg? ',
+    },
+
+    {type: 'input',
+     message:'what is your svg text?',
+     name: 'text',
+    }, 
+
+    {type: 'input',
+     message: 'what is your desired textColor?',
+     name: 'textColor',
+
+    },
+
+])
+.then((answers) => {
+    writeToFile("logo.svg", answers);
+
+})
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, data, 
+        (err) => 
+        err ? console.error(err) : console.log('svg success!'))
+}
+}
+
+init();
